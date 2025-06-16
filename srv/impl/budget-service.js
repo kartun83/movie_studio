e57fc380c9337c8f86247c51cd23455605546825
Tveritinov Aleksei <kartun@yandex.ru>
2025-06-16 13:42:32 -0300

    added before,after,on handlers

--- srv/impl/budget-service.js
+++ srv/impl/budget-service.js
@@ -15,4 +15,22 @@ module.exports = async function(srv) {
 
     return result[0].total || 0;
   });
+
+  // --- FUNCTION: Return budget stats by status ---
+  srv.on('getBudgetStatsFor', async (req) => {
+    const { status } = req.data;
+    const tx = cds.transaction(req);
+
+    const query = SELECT.one
+      .columns([
+        'count(*) as count',
+        'sum(budget) as total',
+        'avg(budget) as avg'
+      ])
+      .from(MovieProject)
+      .where({ status_code: status });
+
+    const result = await tx.run(query);
+    return result || { count: 0, total: 0, avg: 0 };
+  });
 }; 
--- srv/movie-service.js
+++ srv/movie-service.js
@@ -2,12 +2,14 @@ const cds = require('@sap/cds');
 const projectService = require('./impl/project-service');
 const budgetService = require('./impl/budget-service');
 const releasesService = require('./impl/releases-service');
+const movieService = require('./impl/movie-service');
 
 module.exports = async function(srv) {
   // Register all service implementations
   await projectService(srv);
   await budgetService(srv);
   await releasesService(srv);
+  await movieService(srv);
 
   // Log registered handlers for debugging
   console.log('Registered handlers:', Object.keys(srv._handlers));
