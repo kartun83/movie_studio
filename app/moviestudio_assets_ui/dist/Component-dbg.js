74af5867d4be520d0f81379cf3d7d3892c46e9e7
Tveritinov Aleksei <kartun@yandex.ru>
2025-07-24 06:22:46 -0300

    upd

---
+++ app/moviestudio_assets_ui/dist/Component-dbg.js
@@ -0,0 +1,14 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function (BaseComponent) {
+  "use strict";
+
+  /**
+   * @namespace com.kartun.moviestudio.moviestudioassetsui
+   */
+  const Component = BaseComponent.extend("com.kartun.moviestudio.moviestudioassetsui.Component", {
+    metadata: {
+      manifest: "json"
+    }
+  });
+  return Component;
+});
+//# sourceMappingURL=Component-dbg.js.map
---
+++ app/moviestudio_assets_ui/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";const t=e.extend("com.kartun.moviestudio.moviestudioassetsui.Component",{metadata:{manifest:"json"}});return t});
+//# sourceMappingURL=Component.js.map
